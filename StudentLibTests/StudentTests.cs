9bb823abdf7cfdb3a0fb58609154ae565426b309
Thomas (Noit) <tba@live.dk>
2020-08-28 10:12:21 +0200

    Added TestInitialize

--- StudentLibTests/StudentTests.cs
+++ StudentLibTests/StudentTests.cs
@@ -9,35 +9,41 @@ namespace StudentLibTests
     [TestClass()]
     public class StudentTests
     {
-        readonly Student student = new Student("Thomas", "Hedeboparken 4", 3, GenderType.Male);
+        private Student _student;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _student = new Student("Thomas", "Hedeboparken 4", 3, GenderType.Male);
+        }
 
         [TestMethod()]
         public void StudentNameTest()
         {
-            Assert.AreEqual("Thomas", student.Name);
-            Assert.ThrowsException<ArgumentOutOfRangeException>(() => student.Name = "Y");
-            Assert.AreEqual("Thomas", student.Name);
+            Assert.AreEqual("Thomas", _student.Name);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _student.Name = "Y");
+            Assert.AreEqual("Thomas", _student.Name);
         }
 
         [TestMethod]
         public void StudentAddressTest()
         {
-            Assert.AreEqual("Hedeboparken 4", student.Address);
-            Assert.ThrowsException<ArgumentNullException>(() => student.Address = null);
-            Assert.AreEqual("Hedeboparken 4", student.Address);
+            Assert.AreEqual("Hedeboparken 4", _student.Address);
+            Assert.ThrowsException<ArgumentNullException>(() => _student.Address = null);
+            Assert.AreEqual("Hedeboparken 4", _student.Address);
         }
         [TestMethod]
         public void StudentSemesterTest()
         {
-            Assert.AreEqual(3, student.Semester);
-            Assert.ThrowsException<ArgumentOutOfRangeException>(() => student.Semester = 9);
-            Assert.ThrowsException<ArgumentOutOfRangeException>(() => student.Semester = 0);
-            Assert.AreEqual(3, student.Semester);
+            Assert.AreEqual(3, _student.Semester);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _student.Semester = 9);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _student.Semester = 0);
+            Assert.AreEqual(3, _student.Semester);
         }
         [TestMethod]
         public void StudentGenderTest()
         {
-            Assert.AreEqual(GenderType.Male, student.Gender);
+            Assert.AreEqual(GenderType.Male, _student.Gender);
         }
     }
 }
